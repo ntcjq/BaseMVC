45a2b1d0f3bf94fc6aa7486704fd1fc547c47466
ntcjq <754119493@qq.com>
2018-08-07 12:32:41 +0800

    多数据源动态切换 + 分布式事务

--- src/main/java/com/service/TestService.java
+++ src/main/java/com/service/TestService.java
@@ -1,8 +1,11 @@
 package com.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,21 +14,28 @@ import com.dao.multipleDataSource.CustomerContextHolder;
 @Service(value="testService")
 public class TestService {
 
-	@Resource(name="masterJdbcTemplate")
-	private JdbcTemplate masterJdbcTemplate;
+	@Resource(name="sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
 	
-	@Resource(name="slaveJdbcTemplate")
-	private JdbcTemplate slaveJdbcTemplate;
 	
 	
 	
 	@Transactional
 	public void saveMultipleTx() {
-		masterJdbcTemplate.update("insert into dept values (3,'deptNo')");
+		Map dept = new HashMap();
+		dept.put("deptNo", 3);
+		dept.put("deptName", "第一");
+		sqlSessionTemplate.insert("testMapper.testTx",dept);//这是master库的表
 		System.out.println("第一条成功");
-		slaveJdbcTemplate.update("insert into user (nick_name) values ('deptNo')");
+		System.out.println("当前数据源"+CustomerContextHolder.getCustomerType());
+		//切换数据源 参数为配置的key
+		CustomerContextHolder.setCustomerType("ds_slave");
+		System.out.println("当前数据源"+CustomerContextHolder.getCustomerType());
+		Map user = new HashMap();
+		user.put("nick_name", "昵称");
+		sqlSessionTemplate.insert("testMapper.testTx2",user);//这是slave库 的表
 		System.out.println("第二条成功");
-		System.out.println(1/0);
+//		System.out.println(1/0);
 	}
 	
 }
